1ff01ce8a7e74491e05e49f48ef88a525a4ad99b
Paul Demets <paul.demets@hotmail.com>
2022-03-11 17:52:27 +0100

    add requests

--- db.js
+++ db.js
@@ -4,5 +4,9 @@ const store = new DataStore('./data');
 
 module.exports = {
     countries: store.collection('list_pays'),
-    languages: store.collection('list_langues')
+    languages: store.collection('list_langues'),
+    themes: store.collection('list_themes'),
+    sourcesScrap: store.collection('list_sources_scrap'),
+    sources: store.collection('list_sources'),
+    newsArticles: store.collection('newsArticles')
 };
--- resolvers.js
+++ resolvers.js
@@ -1,9 +1,77 @@
 const db = require('./db');
+const { Router } = require('express');
 
 const Query = {
     countries: () => db.countries.list(),
     languages: () => db.languages.list(),
+    themes: () => db.themes.list(),
+    sourcesScrap: () => db.sourcesScrap.list(),
+    sources: () => db.sources.list(),
+    newsArticles: () => db.newsArticles.list()
 }
 
 
-module.exports = { Query }
+
+const Language = {
+    themesByLanguage: (root) => {
+        return db.themes.list().filter(function (item) {
+            return root.token === item.code_langue;
+        });
+    },
+    sourcesScrapByLanguage: (root) => {
+        return db.sourcesScrap.list().filter(function (item) {
+            const source = db.sources.list().find((source) =>
+                source.token == item.token_source
+            );
+            return source.token_langue == root.token;
+        });
+    },
+    newsArticlesByLanguage: (root) => {
+        return db.newsArticles.list().filter(function (item) {
+            const sourceScrap = db.sourcesScrap.list().find((sourceScrap) =>
+                sourceScrap.token == item.token_source_scrap
+            );
+            const source = db.sources.list().find((source) =>
+                source.token == sourceScrap.token_source
+            );
+            return source.token_langue == root.token;
+        });
+    }
+}
+
+const Country = {
+    sourcesScrapByCountry: (root) => {
+        return db.sourcesScrap.list().filter(function (item) {
+            const source = db.sources.list().find((source) =>
+                source.token == item.token_source
+            );
+            return source.token_pays == root.pays;
+        });
+    },
+    newsArticlesByCountry: (root) => {
+        return db.newsArticles.list().filter(function (item) {
+            const sourceScrap = db.sourcesScrap.list().find((sourceScrap) =>
+                sourceScrap.token == item.token_source_scrap
+            );
+            const source = db.sources.list().find((source) =>
+                source.token == sourceScrap.token_source
+            );
+            return source.token_pays == root.token;
+        });
+    },
+}
+
+const Theme = {
+    newsArticlesByTheme: (root) => {
+        return db.newsArticles.list().filter(function (item) {
+            const sourceScrap = db.sourcesScrap.list().find((sourceScrap) =>
+                sourceScrap.token == item.token_source_scrap
+            );
+            return sourceScrap.token_theme == root.token;
+        });
+    },
+}
+
+
+
+module.exports = { Query, Language, Country, Theme }
